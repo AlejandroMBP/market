f81a69d6437de32a0732841d8f2fc8f8b91896be
AlejandroMBP <marcosberriosal@gmail.com>
2026-05-22 23:16:14 -0400

    proveedores, categorias ajuste rutas

--- routes/web.php
+++ routes/web.php
@@ -48,10 +48,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
     Route::get('/proveedores/create', [ProveedorController::class, 'create'])->name('proveedores.create');
     Route::post('/proveedores', [ProveedorController::class, 'store'])->name('proveedores.store');
-    Route::get('/proveedores/{proveedore}', [ProveedorController::class, 'show'])->name('proveedores.show');
-    Route::get('/proveedores/{proveedore}/edit', [ProveedorController::class, 'edit'])->name('proveedores.edit');
-    Route::put('/proveedores/{proveedore}', [ProveedorController::class, 'update'])->name('proveedores.update');
-    Route::delete('/proveedores/{proveedore}', [ProveedorController::class, 'destroy'])->name('proveedores.destroy');
+    Route::get('/proveedores/{proveedor}', [ProveedorController::class, 'show'])->name('proveedores.show');
+    Route::get('/proveedores/{proveedor}/edit', [ProveedorController::class, 'edit'])->name('proveedores.edit');
+    Route::put('/proveedores/{proveedor}', [ProveedorController::class, 'update'])->name('proveedores.update');
+    Route::delete('/proveedores/{proveedor}', [ProveedorController::class, 'destroy'])->name('proveedores.destroy');
+
 
     //rutas de modulo perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
